0439c04ea80b61b1369063bc301a42a0239078ba
novkien <mycompute1102@gmail.com>
2023-06-15 16:14:07 +0800

    adding

---
+++ vendor/composer/installed.php
@@ -0,0 +1,220 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '552766dc48a2cd5652145dc1aa2bd24458317dd1',
+    'name' => 'gempixel/framework',
+  ),
+  'versions' => 
+  array (
+    'abraham/twitteroauth' => 
+    array (
+      'pretty_version' => '3.3.0',
+      'version' => '3.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2e640c1e8bb9b5dbcb2230703f97c89a852e30b5',
+    ),
+    'bacon/bacon-qr-code' => 
+    array (
+      'pretty_version' => '2.0.7',
+      'version' => '2.0.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd70c840f68657ce49094b8d91f9ee0cc07fbf66c',
+    ),
+    'composer/ca-bundle' => 
+    array (
+      'pretty_version' => '1.3.3',
+      'version' => '1.3.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '30897edbfb15e784fe55587b4f73ceefd3c4d98c',
+    ),
+    'dasprid/enum' => 
+    array (
+      'pretty_version' => '1.0.3',
+      'version' => '1.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5abf82f213618696dda8e3bf6f64dd042d8542b2',
+    ),
+    'defuse/php-encryption' => 
+    array (
+      'pretty_version' => 'v2.3.1',
+      'version' => '2.3.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '77880488b9954b7884c25555c2a0ea9e7053f9d2',
+    ),
+    'endroid/qr-code' => 
+    array (
+      'pretty_version' => '4.4.9',
+      'version' => '4.4.9.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bf087fa1e93a1b7310e2d94d187e26ae51db199d',
+    ),
+    'facebook/graph-sdk' => 
+    array (
+      'pretty_version' => '5.7.0',
+      'version' => '5.7.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2d8250638b33d73e7a87add65f47fabf91f8ad9b',
+    ),
+    'gempixel/framework' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '552766dc48a2cd5652145dc1aa2bd24458317dd1',
+    ),
+    'jaybizzle/crawler-detect' => 
+    array (
+      'pretty_version' => 'v1.2.111',
+      'version' => '1.2.111.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd572ed4a65a70a2d2871dc5137c9c5b7e69745ab',
+    ),
+    'maxmind-db/reader' => 
+    array (
+      'pretty_version' => 'v1.11.0',
+      'version' => '1.11.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b1f3c0699525336d09cc5161a2861268d9f2ae5b',
+    ),
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '2.8.0',
+      'version' => '2.8.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '720488632c590286b88b80e62aa3d3d551ad4a50',
+    ),
+    'nikic/fast-route' => 
+    array (
+      'pretty_version' => 'v1.3.0',
+      'version' => '1.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '181d480e08d9476e61381e04a71b34dc0432e812',
+    ),
+    'paragonie/random_compat' => 
+    array (
+      'pretty_version' => 'v9.99.100',
+      'version' => '9.99.100.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '996434e5492cb4c3edcb9168db6fbb1359ef965a',
+    ),
+    'paypal/rest-api-sdk-php' => 
+    array (
+      'pretty_version' => '1.14.0',
+      'version' => '1.14.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '72e2f2466975bf128a31e02b15110180f059fc04',
+    ),
+    'phpfastcache/phpfastcache' => 
+    array (
+      'pretty_version' => '8.1.3',
+      'version' => '8.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'babf654c1618edeb4f9d592d6837b1da661b5107',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.6.3',
+      'version' => '6.6.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9400f305a898f194caff5521f64e5dfa926626f3',
+    ),
+    'psr/cache' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd11b50ad223250cf17b86e38383413f5a6764bf8',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.4',
+      'version' => '1.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+    ),
+    'psr/log-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0.0 || 2.0.0 || 3.0.0',
+      ),
+    ),
+    'psr/simple-cache' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '408d5eafb83c57f6365a3ca330ff23aa4a5fa39b',
+    ),
+    'setasign/fpdf' => 
+    array (
+      'pretty_version' => '1.8.4',
+      'version' => '1.8.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b0ddd9c5b98ced8230ef38534f6f3c17308a7974',
+    ),
+    'sonata-project/google-authenticator' => 
+    array (
+      'pretty_version' => '2.3.1',
+      'version' => '2.3.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '71a4189228f93a9662574dc8c65e77ef55061b59',
+    ),
+    'stripe/stripe-php' => 
+    array (
+      'pretty_version' => 'v7.128.0',
+      'version' => '7.128.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c704949c49b72985c76cc61063aa26fefbd2724e',
+    ),
+  ),
+);
