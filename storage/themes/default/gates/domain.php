1894338d5394ebb28876f3e04ebffbf90e7a2fb4
LE TRUNG KIEN <mycompute1102@gmail.com>
2023-06-15 17:42:21 +0700

    asf

--- storage/themes/default/gates/domain.php
+++ storage/themes/default/gates/domain.php
@@ -1,6 +1,9 @@
 <section>
     <meta http-equiv="Refresh" content="0; url='<?php echo route("home") ?>' />
-    
+    <?php
+header("Location:"+ echo route("home"));
+exit();
+?>
     <div class="container d-flex flex-column">
         <div class="row align-items-center justify-content-between min-vh-100">
             <div class="col-12 col-md-6 col-xl-6 order-md-2">
