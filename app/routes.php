ef931b589e2d412eba08afac2fa6863a000cc2a1
LE TRUNG KIEN <mycompute1102@gmail.com>
2023-06-17 23:10:00 +0700

    12

--- app/routes.php
+++ app/routes.php
@@ -55,7 +55,7 @@ Gem::get('/qr/{id}/download/{format}[/{size}]', 'QR@download')->name('qr.downloa
 
 //// paste form
 Gem::get('/paste', 'Page@paste')->name('paste')->middleware('CheckDomain')->middleware('CheckMaintenance');
-Gem::post('/paste_send', 'Paste@paste_send')->name('paste_send')
+Gem::post('/paste_send', 'Paste@paste_send')->name('paste_send');
 
 
 
