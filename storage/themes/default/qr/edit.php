04d8430c02df7c7504b96c61235767049a3e74fe
LE TRUNG KIEN <mycompute1102@gmail.com>
2023-06-17 08:48:30 +0700

    34

--- storage/themes/default/qr/edit.php
+++ storage/themes/default/qr/edit.php
@@ -200,10 +200,6 @@
                     </div>
                     <div class="card-body">
                         <div id="return-ajax">
-
-                            <a href="<?php echo route('qr.generate', [$qr->alias]) ?>">
-                                <img src="<?php echo route('qr.generate', [$qr->alias]) ?>" class="img-responsive w-100">
-                            </a>
                             <?php
 
                                 try {
@@ -211,10 +207,14 @@
                                 } catch (Exception $e) {
                                     header('Location: '.route('qr.edit', [$qr->id]));
                                 }
-  
+
 
 
                             ?>
+                            <a href="<?php echo route('qr.generate', [$qr->alias]) ?>">
+                                <img src="<?php echo route('qr.generate', [$qr->alias]) ?>" class="img-responsive w-100">
+                            </a>
+
                         </div>    
                         <button type="submit" class="btn btn-primary mt-3"><?php ee('Update') ?></button>
                     </div>
