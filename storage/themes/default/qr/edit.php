a75e74288dce730caf5e2fa48ad27c492f0677bb
LE TRUNG KIEN <mycompute1102@gmail.com>
2023-06-17 16:19:27 +0700

    324

--- storage/themes/default/qr/edit.php
+++ storage/themes/default/qr/edit.php
@@ -202,14 +202,10 @@
                         <div class="card-body">
                             <div id="return-ajax">
                                 <button class="btn btn-primary mb-3" style="padding: 0.2rem" type="button" onclick="window.open('<?php echo route('qr.download', [$qr->alias, 'pdf', 1000]) ?>')"><?php ee("Download as PDF") ?></button>
-<?php
 
-            echo route('home').'/content/qr/'.[$qr->filename][0];
-
-?>
 
                                 <a href="<?php echo route('qr.generate', [$qr->alias]) ?>">
-                                    <img src="<?php echo route('qr.generate', [$qr->alias]) ?>" class="img-responsive w-100" onerror="hideImage(this)" alt="<?php echo [$qr->name] ?>">
+                                    <img src="<?php echo route('home').'/content/qr/'.[$qr->filename][0]?>" class="img-responsive w-100" onerror="hideImage(this)" alt="<?php echo [$qr->name] ?>">
                                 </a>
 
                             </div>    
